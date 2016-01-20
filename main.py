3372f63b11b998abf682c0913815947e0feecc07
Matthew Wo <9029537@gmail.com>
2016-01-19 21:15:54 -0800

    boiler for post xml request

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route("/api/game/<int:game_id>", methods=['GET'])
 def find_game_id(game_id):
-    f = {'key':'hello'}
+    f = {'err':'not implemented'}
+    return flask.jsonify(**f)
+
+@app.route("/api/save", methods=['POST'])
+def save_game_result():
+    f = {'err':'not implemented'}
     return flask.jsonify(**f)
 
 if __name__ == "__main__":
