cbc40b78fdaa082177c21c1da52ea7dbf40607d5
Matthew Wo <9029537@gmail.com>
2016-01-21 15:05:02 -0800

    removed unused codes, added requirement.

--- converter.py
+++ converter.py
@@ -1,11 +1,6 @@
 from xml2json import xml2json
-import time
 
 def converter(xml_file):
-    # use xml2json
+    # returns json in string.
     start_time = time.time()
-    print(xml2json(open(xml_file,'r').read()))
-    print("--- use xml2json takes %s seconds ---" % (time.time() - start_time))
-    
-if __name__ == "__main__":
-    converter('XML Examples/RU50-209-2013-913051-1739668.xml')
+    return xml2json(open(xml_file,'r').read())
