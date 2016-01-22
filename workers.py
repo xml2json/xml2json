335a6a6a3c15d49c01da2b5c82d83f5a1f530e95
jason <huang5640@live.com>
2016-01-22 11:46:07 -0800

    wrote the json2db method
    fix exception handling
    
    add return json to client
    
    fix return json to client
    
    fix return json to client
    
    haha
    
    clean it up
    
    it looks good
    
    it should be good
    
    remove a random file

--- workers.py
+++ workers.py
@@ -1,8 +1,44 @@
 from flask.ext.rq import job
+import couchdb
+import json
 
 @job
 def saveJSON2DB(jsonStr):
-    print("Not implemented! saveJSON2DB")
+	#how to I access the database???
+	couch = couchdb.Server()
+
+	jsonObj = json.loads(jsonStr)
+    
+    ##Because couchdb require that the name of a database to begin with letter 
+	key = 'g' + jsonObj.keys()[0]
+
+	##test if the database with game_id exist
+	try:
+		#could raise ResourceNotFound error
+		db = couch[key]
+	except couchdb.http.ResourceNotFound:
+		#if doesn't exist, create a new database
+		db = couch.create(key)
+
+	db.save(jsonObj.values()[0])
+
+    print("Implemented! saveJSON2DB")
+
+@job
+def returnJSON2Clint(game_id):
+	couch = couchdb.Server()
+	key = 'g' + game_id
+
+	try:
+	    db = couch[key]
+	except couchdb.http.ResourceNotFound:
+		return "Game not Found"
+
+	jsonList = []
+	for id in db:
+	    jsonList.append(db[id]['Game'])
+
+	return jsonList
 
 @job
 def convertXML2JSON(xmlStr):
